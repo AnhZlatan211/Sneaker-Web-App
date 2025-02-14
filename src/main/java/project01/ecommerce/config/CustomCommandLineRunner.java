8259fa3fdfa59ff3c4b75b2484b2bc1b22bb92a9
AnhZlatan <nguyenduyanh2112003@gmail.com>
2025-02-14 14:13:23 +0700

    init Admin

--- src/main/java/project01/ecommerce/config/CustomCommandLineRunner.java
+++ src/main/java/project01/ecommerce/config/CustomCommandLineRunner.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import project01.ecommerce.model.User;
 import project01.ecommerce.repository.UserRepository;
 
 @Configuration
@@ -14,10 +15,23 @@ public class CustomCommandLineRunner{
             System.out.println("🚀🚀🚀 Chạy rồi, tuyệt vời! 🚀🚀🚀");
         };
     }
-    @Bean
-    CommandLineRunner initAdmin(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        return args -> {
-            System.out.println("🚀🚀🚀 Chạy rồi, tuyệt vời! 🚀🚀🚀");
-        };
-    }
+//    @Bean
+//    CommandLineRunner initAdmin(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+//        return args -> {
+//            if (userRepository.findByUsername("admin@gmail.com").isEmpty()) {
+//                User admin = new User();
+//                admin.setUsername("admin@gmail.com");
+//                admin.setPassword(passwordEncoder.encode("1"));
+//                admin.setFullName("Nguyen Duy Anh");
+//                admin.setAge("21");
+//                admin.setGender("GOD!");
+//                admin.setRole("ADMIN");
+//                admin.setEnabled(true);
+//                System.out.println("✅✅✅ User ADMIN đã được tạo. ✅✅✅");
+//            }
+//            else {
+//                System.out.println("❗❗❗ User ADMIN đã tồn tại, không cần thêm. ❗❗❗");
+//            }
+//        };
+//    }
 }
